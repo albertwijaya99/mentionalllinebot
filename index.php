7d7e8d99273be433727bdfaf93353e2899b8c42c
Albert <albetz99@gmail.com>
2020-09-16 18:39:42 +0700

    git test

--- index.php
+++ index.php
@@ -21,17 +21,21 @@ $app->get('/', function ($request, $response) {
 });
 
 $app->post('/', function ($request, $response) {
+	$message = ""
 
-	if (!isset($_SERVER['HTTP_X_HUB_SIGNATURE'])){
+	if (!isset($_SERVER['HTTP_X_HUB_SIGNATURE'])) {
+		$mesage = "HTTP header 'X-Hub-Signature' is missing.";
+	} else {
+		$message = "HTTP header 'X-Hub-Signature' is present";
+	}
 		// init bot
 	$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($_ENV['CHANNEL_ACCESS_TOKEN']);
 	$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $_ENV['CHANNEL_SECRET']]);
 
-	$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello wkkw');
+	$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($message);
 	$response = $bot->pushMessage('U3b5652591281552702e77740cde3a101', $textMessageBuilder);
 
 	echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
-	}
 });
 
 /* JUST RUN IT */
